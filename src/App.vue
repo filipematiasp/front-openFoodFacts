9dd4685b2cb65c52afd8c6f12e4702ba5e3ba1d4
Filipe <filipe.matias@globalbot.com.br>
2022-10-01 08:48:47 -0300

    changes

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <FoodCard />
+</template>
+
+<script>
+import FoodCard from './components/FoodCard.vue'
+
+export default {
+  name: "App",
+  components: {
+    FoodCard
+  }
+}
+</script>
+
+<style>
+
+</style>
